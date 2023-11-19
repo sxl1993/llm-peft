78aec8ebf2b7c39b17065ff63ffdcb0c2e709ab7
sxl1993 <1218197792@qq.com>
2023-11-19 08:29:50 -0800

    add code

--- trainer.py
+++ trainer.py
@@ -23,7 +23,7 @@ from transformers import Trainer
 import torch
 from transformers.modeling_utils import PreTrainedModel, unwrap_model
 from transformers.utils import logging
-
+from peft.peft_model import PeftModel
 logger = logging.get_logger(__name__)
 
 WEIGHTS_NAME = "pytorch_model.bin"
@@ -36,6 +36,7 @@ class PrefixTrainer(Trainer):
         super().__init__(*args, **kwargs)
 
     def _save(self, output_dir: Optional[str] = None, state_dict=None):
+        # breakpoint()
         # If we are executing this function, we are the process zero, so we don't check for that.
         output_dir = output_dir if output_dir is not None else self.args.output_dir
         os.makedirs(output_dir, exist_ok=True)
@@ -47,11 +48,15 @@ class PrefixTrainer(Trainer):
                 if state_dict is None:
                     state_dict = self.model.state_dict()
                 unwrap_model(self.model).save_pretrained(output_dir, state_dict=state_dict)
+            elif isinstance(self.model, PeftModel):
+                if self.save_changed:
+                    logger.info(f"Saving PrefixEncoder to {output_dir}")
+                    self.model.save_pretrained(output_dir)
             else:
                 logger.info("Trainer.model is not a `PreTrainedModel`, only saving its state dict.")
                 if state_dict is None:
                     state_dict = self.model.state_dict()
-                torch.save(state_dict, os.path.join(output_dir, WEIGHTS_NAME))
+                torch.save(state_dict, os.path.join(output_dir, WEIGHTS_NAME)) 
         else:
             if self.save_changed:
                 print("Saving PrefixEncoder")
@@ -68,4 +73,4 @@ class PrefixTrainer(Trainer):
             self.tokenizer.save_pretrained(output_dir)
 
         # Good practice: save your training arguments together with the trained model
-        torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
+        # torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
